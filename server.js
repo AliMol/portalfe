6d2b5ddac240cfe26048d9c08fa11a8bb41dbd7f
NETAPP\moloodia <moloodia@netapp.com>
2018-10-19 10:45:59 +1100

    First Version BoilerPlate

--- server.js
+++ server.js
@@ -9,9 +9,8 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
-  console.log(__dirname);
-  res.sendFile(path.join(__dirname,'/dist/STaaSPortalFE/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
