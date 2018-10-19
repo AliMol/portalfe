412d33a9944cd0bc17b77c8f673b42572a606153
NETAPP\moloodia <moloodia@netapp.com>
2018-10-19 12:54:58 +1100

    First Version BoilerPlate

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -2,10 +2,10 @@ const packageJson = require('../../package.json');
 
 export const environment = {
   appName: 'Angular Ngrx Material Starter',
-  envName: 'PROD',
-  production: true,
-  test: false,
-  i18nPrefix: '/angular-ngrx-material-starter',
+  envName: 'TEST',
+  production: false,
+  test: true,
+  i18nPrefix: '',
   versions: {
     app: packageJson.version,
     angular: packageJson.dependencies['@angular/core'],
